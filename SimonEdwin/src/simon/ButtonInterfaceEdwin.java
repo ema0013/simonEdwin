ad141d0b1a898934c455aaaac189bb066829f238
Student 6 <Student 6@10.8.33.105>
2017-01-10 13:10:23 -0500

    removed setX and setY

--- SimonEdwin/src/simon/ButtonInterfaceEdwin.java
+++ SimonEdwin/src/simon/ButtonInterfaceEdwin.java
@@ -8,10 +8,6 @@ import gui6.components.Clickable;
 public interface ButtonInterfaceEdwin extends Clickable {
 
 	void setColor(Color color);
-
-	void setX(int X);
-
-	void setY(int Y);
 	
 	void setAction(Action a);
 
